c2259dab844dc113a27d7e68d7bb944c104d9af2
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-01 22:25:05 -0600

    Added ReverseTree class

---
+++ src/data-structure/reverse-tree.ts
@@ -0,0 +1,92 @@
+/**
+ * A structure of a tree starting from the leaves, the keys are smallest route without collition.
+ */
+export default class ReverseTree{
+    existing_keys:{[key: string]:FullTreePath} = {};
+    insert(node:KeyList):[inserted:FullTreePath|null,deleted:ChangedPath|null]{
+        let changed_path = null;
+        const new_path = new FullTreePath(node);
+        // get the key for new_path
+        const form_key  = () => new_path.key.join(".");
+        // get minimal path
+        while(  !this.existing_keys[new_path.key.join(".")] &&
+                Object.keys(this.existing_keys).some(key => key.endsWith(new_path.key.join("."))) ){
+            new_path.path_size++;
+        }
+        // check if minimal path exists (solve keys collition)
+        if(this.existing_keys.hasOwnProperty(form_key())){
+            // get path and key for existing keys
+            const old_key = form_key();
+            const old_path = this.existing_keys[form_key()];
+            if(old_path.path.join(".") === new_path.path.join("."))return [null,null];
+            // grab additional key pieces until old_path and new path are different
+            do{
+                old_path.path_size++;
+                new_path.path_size++;
+            }while(old_path.key.join(".")===new_path.key.join("."));
+            //replace the old_key with the differentiation
+            delete this.existing_keys[old_key];
+            this.existing_keys[old_path.key.join(".")] = old_path;
+            changed_path = {
+                old_key,
+                new_key:old_path.key.join("."),
+                path:old_path
+            }
+        }
+        // add the new path
+        this.existing_keys[form_key()] = new_path;
+        return [
+            this.existing_keys[form_key()],
+            changed_path
+        ];
+    }
+    find(node:KeyList): FullTreePath | undefined{
+        const old_path = Object.values(this.existing_keys).find((path)=>path.path.join(".")===node.join(".")||path.key.join(".")===node.join("."));
+        if(!old_path) return;
+        return old_path;
+    }
+    remove(node:KeyList):[ removed: FullTreePath | null, changed: ChangedPath | null]{
+        const old_path = this.find(node);
+        if(!old_path) return[ null, null];
+        //remove the old path
+        delete this.existing_keys[old_path.key.join(".")];
+        // copy the old path to prevent modifying the old path
+        const copy_old_path = Object.create(old_path);
+        // reduce the path_size to find the collitioned_paths
+        copy_old_path.path_size--;
+        const collitioned_paths = Object.values(this.existing_keys).filter(path => path!==old_path && path.key.join(".").endsWith(copy_old_path.key.join(".")));
+        if(collitioned_paths.length > 1 || collitioned_paths.length === 0) return [old_path,null];
+        const collitioned_path = collitioned_paths[0];
+        const old_collitioned_key = collitioned_path.key.join(".")
+        delete this.existing_keys[old_collitioned_key];
+        // reduce path_size until we find a collition
+        while(!Object.values(this.existing_keys).some(path => path.path.join(".").endsWith(collitioned_path.key.join("."))) && collitioned_path.path_size>=1)
+            collitioned_path.path_size--;
+        // remove the collition
+        collitioned_path.path_size++;
+        this.existing_keys[collitioned_path.key.join(".")] = collitioned_path;
+        return [
+            old_path,
+            {
+                old_key:old_collitioned_key,
+                new_key:collitioned_path.key.join("."),
+                path: collitioned_path
+            }];
+    }
+    get keys():KeyList[]{
+        return Object.entries(this.existing_keys).map(([_,path]) => path.key);
+    }
+}
+class FullTreePath{
+    path_size:number=1;
+    constructor(public path:KeyList){}
+    get key(){ return this.path.slice(-this.path_size); }
+}
+export type TreeNode = { [key:KeyPiece]:TreeNode};
+export type KeyList = KeyPiece[];
+export type KeyPiece = number|string;
+export type ChangedPath = {
+    old_key:string;
+    new_key:string;
+    path:FullTreePath;
+}
---
+++ src/test/suite/reverse-tree.ts.test.ts
@@ -0,0 +1,111 @@
+import * as assert from 'assert';
+import { compareArray, shuffle } from '../../data-structure/ArrayFunctions';
+import ReverseTree, { KeyList } from '../../data-structure/reverse-tree';
+
+const file_paths = [
+    ["a","z"],
+    ["a","c","e","d","z"],
+    ["a","b","d","z"],
+    ["a","b","e","d","z"],
+    ["a","c","z"],
+]
+Object.freeze(file_paths);
+suite('ReverseTree', () => {
+    test("insert in tree",()=>{
+        const tree = new ReverseTree();
+        tree.insert(["a","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["z"]]),true,`check after insert ${["a","z"]} failed.`);
+        tree.insert(["a","c","e","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["d","z"]]),true,`check after insert ${["a","c","e","d","z"]} failed.`);
+        tree.insert(["a","b","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["e","d","z"],["b","d","z"]]),true,`check after insert ${["a","b","d","z"]} failed.`);
+        tree.insert(["a","b","e","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["b","e","d","z"],["b","d","z"],["c","e","d","z"]]),true,`check after insert ${["a","b","e","d","z"]} failed.`);
+        tree.insert(["a","c","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["b","d","z"],["b","e","d","z"],["c","e","d","z"],["c","z"]]),true,`check after insert ${["a","c","z"]} failed.`);
+    })
+    test("insert order does not matter",()=>{
+        const paths = [...file_paths];
+        for (let index = 0; index < 10; index++) {
+            const tree = new ReverseTree()
+            shuffle(paths);
+            for(const path of paths){
+                tree.insert(path);
+            }
+            assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["b","d","z"],["b","e","d","z"],["c","e","d","z"],["c","z"]]),true);
+        }
+    })
+    test("does not contain duplicates",()=>{
+        const tree = new ReverseTree()
+        for(const path of file_paths){
+            tree.insert(path);
+            const [ new_path,old_path ] = tree.insert(path);
+            assert.strictEqual(new_path,null);
+            assert.strictEqual(old_path,null);
+        }
+        const paths = tree.keys;
+        for(const compare_path of paths){
+            let count_equals = 0;
+            paths.forEach(path =>{if(compareArray(path,compare_path)){count_equals++;}});
+            assert.strictEqual(count_equals,1);
+        }
+    })
+    test("remove from tree in inserting order", () =>{
+        const tree = new ReverseTree();
+        tree.insert(["a","z"]);
+        tree.insert(["a","c","e","d","z"]);
+        tree.insert(["a","b","d","z"]);
+        tree.insert(["a","b","e","d","z"]);
+        tree.insert(["a","c","z"]);
+        //start removing
+        tree.remove(["a","c","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["b","e","d","z"],["b","d","z"],["c","e","d","z"]]),true,`check after removing ${["a","c","z"]} failed.`);
+        tree.remove(["a","b","e","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["e","d","z"],["b","d","z"]]),true,`check after removing ${["a","b","e","d","z"]} failed.`);
+        tree.remove(["a","b","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["d","z"]]),true,`check after removing ${["a","b","d","z"]} failed.`);
+        tree.remove(["a","c","e","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["z"]]),true,`check after removing ${["a","c","e","d","z"]} failed.`);
+    })
+    test("remove from tree in diferent order", () =>{
+        const tree = new ReverseTree();
+        tree.insert(["a","z"]);
+        tree.insert(["a","c","e","d","z"]);
+        tree.insert(["a","b","d","z"]);
+        tree.insert(["a","b","e","d","z"]);
+        tree.insert(["a","c","z"]);
+
+        tree.remove(["a","b","d","z"])
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["b","e","d","z"],["c","e","d","z"],["c","z"]]),true,`remove ${["a","b","d","z"]} should not remove collition`);
+        tree.remove(["a","b","e","d","z"]);
+        assert.strictEqual(assertIncludesAll(tree.keys,[["a","z"],["d","z"],["c","z"]]),true,`remove ${["a","b","e","d","z"]} should remove 2 levels in ${["c","e","d","z"]}`);
+    })
+    test("find from reverse tree",()=>{
+        const tree = new ReverseTree();
+        tree.insert(["a","z"]);
+        tree.insert(["a","c","e","d","z"]);
+        tree.insert(["a","b","d","z"]);
+        tree.insert(["a","b","e","d","z"]);
+        tree.insert(["a","c","z"]);
+
+        assert.strictEqual(tree.find(["z"]),undefined);
+        assert.notStrictEqual(tree.find(["a","z"]),undefined);
+        assert.strictEqual(tree.find(["e","d","z"]),undefined);
+        assert.notStrictEqual(tree.find(["b","e","d","z"]),undefined);
+        assert.notStrictEqual(tree.find(["a","b","e","d","z"]),undefined);
+    })
+})
+
+function assertIncludesAll(actual:KeyList[], expected:KeyList[]){
+    return  actual
+            .every( path=>
+                expected
+                .some(arr=>compareArray(path,arr))
+            ) &&
+            expected
+            .every( path=>
+                actual
+                .some(arr=>compareArray(path,arr))
+            ); 
+}
+

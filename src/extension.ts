1764bf10db3860a773ae3b03e1918db1d3a3f148
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-03 14:32:31 -0600

    Update extension.ts
    -Added Semantic Token Provider to highlight sintaxis
    -Extracted document selector for language providers.

--- src/extension.ts
+++ src/extension.ts
@@ -7,6 +7,8 @@ import createTemplate from './commands/create-template';
 import copyTemplate from './commands/copy-template';
 import CardReferenceCompleter from './language-features/complete-name';
 import CardReferenceLink from './language-features/link-name';
+import HighlightSintaxis from './language-features/highlight-name';
+import Tokenizer from './language-features/tokenizer';
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
@@ -22,8 +24,10 @@ export async function activate(context: vscode.ExtensionContext) {
 	context.subscriptions.push(vscode.commands.registerCommand('world-build.create', createFromTemplate));
 	context.subscriptions.push(vscode.commands.registerCommand('world-build.create-template', createTemplate));
 	context.subscriptions.push(vscode.commands.registerCommand('world-build.copy-template', copyTemplate));
-	context.subscriptions.push(vscode.languages.registerCompletionItemProvider({ language:'yaml', scheme: 'file'}, new CardReferenceCompleter(),'@'));
-	context.subscriptions.push(vscode.languages.registerDocumentLinkProvider({ language:'yaml', scheme: 'file'}, new CardReferenceLink()));
+	const selector = { language:'yaml', scheme: 'file'};
+	context.subscriptions.push(vscode.languages.registerCompletionItemProvider(selector, new CardReferenceCompleter(),'@'));
+	context.subscriptions.push(vscode.languages.registerDocumentLinkProvider(selector, new CardReferenceLink()));
+	context.subscriptions.push(vscode.languages.registerDocumentSemanticTokensProvider(selector, new HighlightSintaxis(), Tokenizer.legend));
 	setTimeout(() => {
 		console.log(contentCards);
 	}, 1500);

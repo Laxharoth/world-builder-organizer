ddb2dfb80dcffbd7ed764f90bcf6e5cffec66722
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-02 22:16:13 -0600

    Added KEY_PATH_SEPARATOR constant
    Added a constant to separate the directories in the path of the files.

--- src/data-structure/reverse-tree.ts
+++ src/data-structure/reverse-tree.ts
@@ -1,3 +1,5 @@
+import { KEY_PATH_SEPARATOR } from "../constant";
+
 /**
  * A structure of a tree starting from the leaves, the keys are smallest route without collition.
  */
@@ -11,8 +13,8 @@ export default class ReverseTree{
         let changedPath = null;
         const newPath = new FullTreePath(node);
         // get minimal path
-        while(  !this.existingKeys[newPath.key.join(".")] &&
-                Object.keys(this.existingKeys).some(key => key.endsWith(newPath.key.join("."))) ){
+        while(  !this.existingKeys[newPath.strKey] &&
+                Object.keys(this.existingKeys).some(key => key.endsWith(newPath.strKey)) ){
             newPath.pathSize++;
         }
         // check if minimal path exists (solve keys collition)
@@ -20,18 +22,18 @@ export default class ReverseTree{
             // get path and key for existing keys
             const oldKey = newPath.strKey;
             const oldPath = this.existingKeys[newPath.strKey];
-            if(oldPath.path.join(".") === newPath.path.join(".")){return [null,null];}
+            if(oldPath.strPath === newPath.strPath){return [null,null];}
             // grab additional key pieces until oldPath and new path are different
             do{
                 oldPath.pathSize++;
                 newPath.pathSize++;
-            }while(oldPath.key.join(".")===newPath.key.join("."));
+            }while(oldPath.strKey===newPath.strKey);
             //replace the oldKey with the differentiation
             delete this.existingKeys[oldKey];
-            this.existingKeys[oldPath.key.join(".")] = oldPath;
+            this.existingKeys[oldPath.strKey] = oldPath;
             changedPath = {
                 oldKey: oldKey,
-                newKey:oldPath.key.join("."),
+                newKey:oldPath.strKey,
                 path:oldPath
             };
         }
@@ -43,7 +45,7 @@ export default class ReverseTree{
         ];
     }
     find(node:KeyList): FullTreePath | undefined{
-        const oldPath = Object.values(this.existingKeys).find((path)=>path.path.join(".")===node.join(".")||path.key.join(".")===node.join("."));
+        const oldPath = Object.values(this.existingKeys).find((path)=>path.strPath===node.join(KEY_PATH_SEPARATOR)||path.strKey===node.join(KEY_PATH_SEPARATOR));
         if(!oldPath) {return;}
         return oldPath;
     }
@@ -52,27 +54,27 @@ export default class ReverseTree{
         const oldPath = this.find(node);
         if(!oldPath) {return[ null, null];}
         //remove the old path
-        delete this.existingKeys[oldPath.key.join(".")];
+        delete this.existingKeys[oldPath.strKey];
         // copy the old path to prevent modifying the old path
         const copyOldPath = Object.create(oldPath);
         // reduce the path_size to find the collitioned_paths
         copyOldPath.path_size--;
-        const collitionedPaths = Object.values(this.existingKeys).filter(path => path!==oldPath && path.key.join(".").endsWith(copyOldPath.key.join(".")));
+        const collitionedPaths = Object.values(this.existingKeys).filter(path => path!==oldPath && path.strKey.endsWith(copyOldPath.strKey));
         if(collitionedPaths.length > 1 || collitionedPaths.length === 0) {return [oldPath,null];}
         const collitionedPath = collitionedPaths[0];
-        const oldCollitionedKey = collitionedPath.key.join(".");
+        const oldCollitionedKey = collitionedPath.strKey;
         delete this.existingKeys[oldCollitionedKey];
         // reduce path_size until we find a collition
-        while(!Object.values(this.existingKeys).some(path => path.path.join(".").endsWith(collitionedPath.key.join("."))) && collitionedPath.pathSize>=1)
+        while(!Object.values(this.existingKeys).some(path => path.strPath.endsWith(collitionedPath.strKey)) && collitionedPath.pathSize>=1)
             {collitionedPath.pathSize--;}
         // remove the collition
         collitionedPath.pathSize++;
-        this.existingKeys[collitionedPath.key.join(".")] = collitionedPath;
+        this.existingKeys[collitionedPath.strKey] = collitionedPath;
         return [
             oldPath,
             {
                 oldKey:oldCollitionedKey,
-                newKey:collitionedPath.key.join("."),
+                newKey:collitionedPath.strKey,
                 path: collitionedPath
             }];
     }
@@ -88,14 +90,15 @@ export default class ReverseTree{
         if(!this.modified){return;}
         this.modified = false;
         this.memoizedKeys = Object.entries(this.existingKeys).map(([_,path]) => path.key);
-        this.memoizedPaths=this.memoizedKeys.map(key=>key.join('.'));
+        this.memoizedPaths= Object.entries(this.existingKeys).map(([_,path]) => path.strKey);
     }
 }
 class FullTreePath{
     pathSize:number=1;
     constructor(public path:KeyList){}
     get key(){ return this.path.slice(-this.pathSize); }
-    get strKey(){ return this.key.join("."); }
+    get strKey(){ return this.key.join(KEY_PATH_SEPARATOR).split('.').slice(0,-1).join(""); }
+    get strPath(){ return this.path.join(KEY_PATH_SEPARATOR); }
 }
 export type TreeNode = { [key:KeyPiece]:TreeNode};
 export type KeyList = KeyPiece[];

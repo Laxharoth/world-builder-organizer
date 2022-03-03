b8c0434a0dca49b7a7315539ba0d41cdcfd106ea
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-03 15:54:03 -0600

    Update link-name.ts
    CardReferenceLink extends Tokenizer

--- src/language-features/link-name.ts
+++ src/language-features/link-name.ts
@@ -3,23 +3,16 @@ import {
     DocumentLinkProvider, DocumentLink, ProviderResult, TextDocument, Range, Uri
 } from "vscode";
 import { contentCards } from "../load-data/map-content";
+import Tokenizer from "./tokenizer";
 
-export default class CardReferenceLink implements DocumentLinkProvider{
+export default class CardReferenceLink extends Tokenizer implements DocumentLinkProvider{
     provideDocumentLinks(document: TextDocument, token: CancellationToken): ProviderResult<DocumentLink[]> {
-        const text = document.getText();
         const links : DocumentLink[]=[];
-        Array.from(new Set(text.split(/(\s|\t|\n)+/).filter(word=>word.startsWith("@") && word!=="@"))).forEach(word=>{
-            let index = 0;
-            const link = contentCards[word.substring(1)];
-            if(!link){return;}
+        for(const linkToken of this.tokenize(document).filter(token=>token.tokenType==='link')){
+            const link = contentCards[linkToken.token.substring(1)];
             const linkData = Uri.file(link.path);
-            while( (index = text.indexOf(word,index)) >= 0){
-                const start = document.positionAt(index);
-                const end   = document.positionAt(index+word.length);
-                links.push( new DocumentLink(new Range(start, end),linkData));
-                index++;
-            }
-        });
+            links.push( new DocumentLink(linkToken.range,linkData));
+        }
         return links;
     }
 }

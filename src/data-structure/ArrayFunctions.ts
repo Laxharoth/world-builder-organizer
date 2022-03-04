aacac6cf706f4222381ca7bac50cc6fe1706a2b2
Laxharoth <zeydenlaxharoth@gmail.com>
2022-03-03 20:42:37 -0600

    Fixed ESLint

--- src/data-structure/ArrayFunctions.ts
+++ src/data-structure/ArrayFunctions.ts
@@ -3,7 +3,7 @@ import { randomInt } from "crypto";
 export function compareArray<T>(array1: T[], array2: T[], compareValues:(value: T, value2: T)=>boolean=(v1,v2)=>v1===v2): boolean {
     try {
         const zipped = zip(array1, array2);
-        if(zipped.some(([v1,v2])=>!compareValues(v1,v2)))return false;
+        if(zipped.some(([v1,v2])=>!compareValues(v1,v2))){return false;}
     }catch (e) {
         return false;
     }
@@ -11,20 +11,20 @@ export function compareArray<T>(array1: T[], array2: T[], compareValues:(value: 
 }
 export function zip<T>(...arrays:T[][]):T[][]{
     const result:T[][] = [];
-    if(!arrays.length)throw new Error("Provide at least an array");
+    if(!arrays.length){throw new Error("Provide at least an array");}
     let length = arrays[0].length;
-    if(arrays.some(arr => arr.length!==length))throw new Error("Lenght on arrays must be equal");
+    if(arrays.some(arr => arr.length!==length)){throw new Error("Lenght on arrays must be equal");}
     for (let i = 0; i < length; i++) {
-        result.push([])
+        result.push([]);
         for (let j = 0; j < arrays.length; j++){
-            result[i].push(arrays[j][i])
+            result[i].push(arrays[j][i]);
         }
     }
     return result;
 }
 export function shuffle<T>(array: T[]){
     for (let i = 0; i < array.length; i++){
-        const swap_index = randomInt(array.length - i);
-        [ array[i],array[swap_index] ] = [ array[swap_index],array[i] ]
+        const swapIndex = randomInt(array.length - i);
+        [ array[i],array[swapIndex] ] = [ array[swapIndex],array[i] ];
     }
 }
--- src/parser.ts
+++ src/parser.ts
@@ -5,46 +5,46 @@ export const files: FileNames = {};
 export const variables:{ [key : string]: string|null} = {
     name:null,
     description:"description"
-}
+};
 export async function setFileContent(name: string, content: string) {
-    if(files)await loadFiles(files);
+    if(files){await loadFiles(files);}
     if (/^@/.test(name)) {
         name = name.replace(/^@/, '');
-        const [file_content,file_name] = splitFirst(name, ':');
-        name = file_name || file_content;
-        content = files[file_content as FileName];
+        const [fileContent,fileName] = splitFirst(name, ':');
+        name = fileName || fileContent;
+        content = files[fileContent as FileName];
     }
     return { name, content };
 }
-const regex_find_variable = /\[.+\]/;
+const regexFindVariable = /\[.+\]/;
 export function parseVariable(name: string) {
-    while(regex_find_variable.test(name)){
-        regex_find_variable.exec(name)
+    while(regexFindVariable.test(name)){
+        regexFindVariable.exec(name)
             ?.forEach(str=>{
-                const variable_name = str.substring(1,str.length - 1);
-                name = name.replace(str,variables[variable_name] || variable_name);
+                const variableName = str.substring(1,str.length - 1);
+                name = name.replace(str,variables[variableName] || variableName);
             });
     }
     return name;
 }
 export function extractContentVariables(content:string){
-    const variables_names:string[] = []
-    while(regex_find_variable.test(content))
-        regex_find_variable.exec(content)
+    const variablesNames:string[] = [];
+    while(regexFindVariable.test(content))
+        {regexFindVariable.exec(content)
             ?.forEach(str=>{ 
                 content = content.replace(str,'');
-                variables_names.push(str.substring(1,str.length - 1));
-            });
-    return variables_names;
+                variablesNames.push(str.substring(1,str.length - 1));
+            });}
+    return variablesNames;
 }
 function splitFirst(str:string,separator:string):[string,string|null] {
     let [first, ...rest] = str.split(separator);
     return [first, rest.length?rest.join(separator):null];
 }
 export async function setVariable(name:string, placeHolder:string|null=null, value:string|null=null, prompt:string|null=null){
-    if(!placeHolder){ placeHolder = `${name}:`};
-    if(!prompt){ prompt =`Enter value for variable ${name}`};
-    if(!value)value='';
+    if(!placeHolder){ placeHolder = `${name}:`;};
+    if(!prompt){ prompt =`Enter value for variable ${name}`;};
+    if(!value){value='';}
     value = await vscode.window.showInputBox({ placeHolder, value, prompt }) || null;
-    if(value)variables[name] = value;
+    if(value){variables[name] = value;}
 }

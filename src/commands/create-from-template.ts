687c048f8a18e4eda1098cb2625f5fb279ff5050
Luis Daniel Espino Mandujano <zeydenlaxharoth@gmail.com>
2025-01-09 17:44:20 -0600

    Added tool to allow use of Variables in template files

--- src/commands/create-from-template.ts
+++ src/commands/create-from-template.ts
@@ -2,6 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
+import {META_VARIABLES} from '../constant';
 import createFile, {CreateFileOptions} from '../create-file/create-file';
 import {templateCards} from '../load-data/map-content';
 import {extractContentVariables, setVariable, variables} from '../parser';
@@ -18,13 +19,15 @@ export default async function createFromTemplate() {
     if (!filePath) {
         return;
     }
-    let content;
+    let readFile;
     try {
-        content = (await fs.promises.readFile(card.path)).toString();
+        readFile = (await fs.promises.readFile(card.path)).toString();
     } catch (error) {
         vscode.window.showErrorMessage(`${error}`);
         return;
     }
+    const {result: metaVariables, updatedContent: content} =
+        extractMetaVariables(readFile);
     const contentVariables = extractContentVariables(content);
     for (const variable of contentVariables) {
         if (variable === '@name')
@@ -32,7 +35,8 @@ export default async function createFromTemplate() {
         else
             await setVariable(variable);
     }
-    const pathSubdirs = `${filePath}.yml`.split('/');
+    const {relativePath = '.'} = metaVariables;
+    const pathSubdirs = `${relativePath}/${filePath}.yml`.split('/');
     let currentPath = 'world';
     const files: CreateFileOptions[] = [];
     for (let index = 0; index < pathSubdirs.length; index++) {
@@ -46,3 +50,33 @@ export default async function createFromTemplate() {
         createFile(files);
     }
 }
+
+function extractMetaVariables(content: string):
+    {result: Record<string, string|boolean>, updatedContent: string} {
+    // Initialize the result object
+    const result: Record<string, string|boolean> = {};
+
+    // Replace matches in content while processing
+    const updatedContent =
+        content.replace(META_VARIABLES, (fullMatch, innerContent) => {
+            if (fullMatch === '[@name]') {
+                // Preserve [@name] in the content
+                return fullMatch;
+            }
+
+            const [key, value] =
+                innerContent.split('=').map((str: string) => str.trim());
+
+            if (key && value) {
+                // Add the key-value pair to the result
+                result[key] = value;
+            } else {
+                result[key] = true;
+            }
+
+            // Remove the match from the content
+            return '';
+        });
+
+    return {result, updatedContent};
+};
--- src/constant.ts
+++ src/constant.ts
@@ -1,3 +1,4 @@
 export const KEY_PATH_SEPARATOR = '_';
 export const LINK_STARTING_CHARACTER = '+';
 export const TOKEN_SEPARATOR = /[\s\t\n[\]{}()*?.,^$|#\s<>:!;¡¿~]/g;
+export const META_VARIABLES = /\[@(.*?)\]/g;
